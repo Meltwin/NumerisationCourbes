1cf08cdaf9717f1f10b4fd0306bb94174a07c97c
Meltwin <meltwin158@gmail.com>
2021-03-21 00:51:03 +0100

    Adding process to the testing file

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from Processing import *
-from Processing.color import Isolator
-from Processing.select import Selector, SelOption
 import matplotlib.pyplot as plt
 
-from Processing.sort import Sorting, SortOrder, _sort_asc
+from Processing import *
+from Processing.color import *
+from Processing.select import *
+from Processing.sort import *
+from Processing.io import *
 
 COLOR = (.29019, .85882, .705882)
 COLOR_DIST = .38
 
-proc = Pipeline()
+# Image Process
 load = ImageLoader("./imgs/c0_redu.png")
 load.run()
 
@@ -19,14 +20,30 @@ s = Selector()
 s.add_selection(((0, 0), (600, 330)), SelOption.INCLUDE)
 s.run(i.get_img())
 
+# Point Process
+pt = ImageToPoint()
+pt.run(s.get_img())
+
+sort = Sorting(SortOrder.CYCLE)
+sort.run(pt.get_points())
+
+expo = CSVExport("./out/c0.csv")
+expo.run(sort.get_points())
 
 plt.imshow(load.get_img(), cmap="gray")
 plt.imshow(s.get_img(), cmap="gray")
 plt.show()
 
-so = Sorting(SortOrder.CYCLE)
-so.run([(0, 1)])
 
-print(type(_sort_asc))
+def getXY() -> Tuple[List[int], List[int]]:
+    X, Y = [], []
+    for (x, y) in sort.get_points():
+        X.append(x)
+        Y.append(y)
+    return X, Y
 
+
+X, Y = getXY()
+plt.plot(X, Y)
+plt.show()
 
